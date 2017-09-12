334972ed50eab4ef02d1d99bce01af028409e358
oleggalimov <o.d.galimov@yandex.ru>
2017-09-12 21:37:54 +0500

    Добавлены меню на главном экране

--- src/guiComponents/mainWindow.java
+++ src/guiComponents/mainWindow.java
@@ -3,6 +3,7 @@ package guiComponents;
 import guiComponents.menuBarComponents.AboutIyem.aboutItem;
 import guiComponents.menuBarComponents.FilItem.fileItem;
 import javax.swing.*;
+import java.awt.*;
 
 
 /**
@@ -17,9 +18,48 @@ public class mainWindow extends JFrame {
         JFrame mainWindow = new JFrame();
         mainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
         mainWindow.setTitle("Управление материалами");
-        mainWindow.setSize(400,300);
+        mainWindow.setSize(600,500);
         mainWindow.setLocationRelativeTo(null);
         mainWindow.setJMenuBar(createMenuPanel());
+        Container mainContainer = mainWindow.getContentPane();
+        mainContainer.setLayout(new BoxLayout(mainContainer,BoxLayout.LINE_AXIS));//устанавливаем выравнивание
+
+        //левая панель
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel,BoxLayout.Y_AXIS));
+
+        JTextField nameOfMaterial = new JTextField("Материал");
+        nameOfMaterial.setBorder(BorderFactory.createTitledBorder("Наименование материала"));
+
+        JTextField weigthOfMaterial = new JTextField("0.0");
+        weigthOfMaterial.setBorder(BorderFactory.createTitledBorder("Удельный вес"));
+
+        JTextField isConductor = new JTextField("0");
+        isConductor.setBorder(BorderFactory.createTitledBorder("Свойство проводимости"));
+
+        leftPanel.add(nameOfMaterial);
+        leftPanel.add(weigthOfMaterial);
+        leftPanel.add(isConductor);
+
+        //правая панель
+        JPanel rihthPAnel = new JPanel();
+        //rihthPAnel.setLayout(new BoxLayout(rihthPAnel,BoxLayout.Y_AXIS));
+        Box box = Box.createVerticalBox();
+
+        JButton addMaterial = new JButton("Добавить");
+        addMaterial.setAlignmentY(JComponent.CENTER_ALIGNMENT);
+        JButton clearForm = new JButton("Очистить");
+        clearForm.setAlignmentY(JComponent.CENTER_ALIGNMENT);
+        box.add(addMaterial);
+        box.add(Box.createVerticalStrut(10));
+        box.add(clearForm);
+
+        rihthPAnel.add(box);
+        mainContainer.add(leftPanel);
+        mainContainer.add(rihthPAnel);
+
+
+
         mainWindow.setVisible(true);
     }
     private JMenuBar createMenuPanel () {
--- src/main.java
+++ src/main.java
@@ -4,15 +4,23 @@ import model.material;
 import java.io.*;
 import java.util.List;
 
+/*
+1.	Сведения о веществе: название (строка), удельный вес (вещественное число),
+свойство проводимости (целое число: 0 – нет проводимости, 1 – полупроводник,
+2 – проводник По сведениям о некотором количестве веществ получить список веществ ,
+название которых состоит не менее, чем из двух слов, отсортированных по возрастанию удельных весов.
+Если  нужных веществ   нет, выдать соответствующее сообщение.
+
+Написать графический интерфейс для этой задачи
+
+*/
 
 /**
  * Created by oleg on 09.09.17.
  */
 public class main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        List<material> mainList;
         mainWindow mainWindow = new mainWindow();
-
 
     }
 }
